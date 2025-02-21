933bbea6811d09197e8c37333c8bc9e43d660f0e
Doyal Saji Vithayathil <150351001+Doyal-Saji-Vithayathil@users.noreply.github.com>
2025-02-22 00:41:27 +0530

    edited

--- app/layout.js
+++ app/layout.js
@@ -3,9 +3,6 @@ import "./globals.css";
 import Header from "@/components/header";
 import { ClerkProvider } from "@clerk/nextjs";
 import { Toaster } from "@/components/ui/sonner";
-import { Analytics } from "@vercel/analytics/react"
-import { SpeedInsights } from "@vercel/speed-insights/next"
-
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
@@ -29,9 +26,7 @@ export default function RootLayout({ children }) {
             <div className="container mx-auto px-4 text-center text-gray-600">
               <p> Made by Doyal Saji </p>
             </div>
-          </footer>
-          <SpeedInsights />
-          <Analytics />
+          </footer>          
         </body>
       </html>
     </ClerkProvider>
