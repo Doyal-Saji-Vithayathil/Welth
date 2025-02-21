29a0302f5124dde74d06ad84313025cec316100b
Doyal Saji Vithayathil <150351001+Doyal-Saji-Vithayathil@users.noreply.github.com>
2025-02-21 19:28:53 +0530

    added Generate Monthly Insigts

--- app/api/inngest/route.js
+++ app/api/inngest/route.js
@@ -1,17 +1,20 @@
 import { inngest } from "@/lib/inngest/client";
 import {
   checkBudgetAlert,
+  generateMonthlyReports,
   processRecurringTransaction,
   triggerRecurringTransaction,
 } from "@/lib/inngest/functions";
 
 import { serve } from "inngest/next";
 
+//create an API that serves zero function
 export const { GET, POST, PUT } = serve({
   client: inngest,
   functions: [
     checkBudgetAlert,
     triggerRecurringTransaction,
     processRecurringTransaction,
-  ], // Ensure functions are correctly passed
+    generateMonthlyReports,
+  ], 
 });
